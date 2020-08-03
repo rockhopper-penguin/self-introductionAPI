d63601bcfd51cd4172fbff6d68fd443f6e8595f5
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-08-03 22:06:09 +0900

    corsの読み込み

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
 import express from "express";
 import * as fs from "fs";
 import moment from "moment-timezone";
+import cors from "cors";
 
 const app: express.Express = express();
 let datas: any;
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", cors(), (req: express.Request, res: express.Response) => {
   return res.send("Communication successful!");
 });
 
-app.get("/data", async (req: express.Request, res: express.Response) => {
-  try {
-    datas = await JSON.parse(fs.readFileSync("./json/profile.json", "utf-8"));
-    datas.acquisition_time = moment()
-      .tz("Asia/Tokyo")
-      .format("YYYY-MM/DD hh:mm:ss");
-  } catch (e) {
-    datas = ["データ取得エラー！", e];
+app.get(
+  "/data",
+  cors(),
+  async (req: express.Request, res: express.Response) => {
+    try {
+      datas = await JSON.parse(fs.readFileSync("./json/profile.json", "utf-8"));
+      datas.acquisition_time = moment()
+        .tz("Asia/Tokyo")
+        .format("YYYY-MM/DD hh:mm:ss");
+    } catch (e) {
+      datas = ["データ取得エラー！", e];
+    }
+    return res.send(JSON.stringify(datas));
   }
-  return res.send(JSON.stringify(datas));
-});
+);
 
 app.listen(3000);
 
