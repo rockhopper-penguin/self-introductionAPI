e80aa6685c3deda47e42e8839c514be921e71b81
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-10-11 22:16:55 +0900

    文字列出力の修正

--- go/go_lang/index.go
+++ go/go_lang/index.go
@@ -6,6 +6,6 @@ import "net/http"
 
 
 func main() {
-	fmt.Printf("Server runngin -> http://127.0.0.1:8080/\n")
+	fmt.Println("Server runngin -> http://127.0.0.1:8080/")
 	http.ListenAndServe(":8080", nil)
 }
