436ae56c6f1af98e1796fc89140245f39f5b7688
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-10-14 21:55:34 +0900

    webサーバの処理追加

--- go/go_lang/index.go
+++ go/go_lang/index.go
@@ -7,5 +7,8 @@ import "net/http"
 
 func main() {
 	fmt.Println("Server runngin -> http://127.0.0.1:8080/")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Rockhopper-Penguin")
+	})
 	http.ListenAndServe(":8080", nil)
 }
