d97cf317c71e0ecfa25b3ffa1c56624dfaf881e6
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-10-14 22:00:23 +0900

    修正

--- go/go_lang/index.go
+++ go/go_lang/index.go
@@ -4,11 +4,13 @@ import "fmt"
 // import "log"
 import "net/http"
 
+func response(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Rockhopper-Penguin")
+  }
+
 
 func main() {
 	fmt.Println("Server runngin -> http://127.0.0.1:8080/")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "Rockhopper-Penguin")
-	})
+	http.HandleFunc("/", response)
 	http.ListenAndServe(":8080", nil)
 }
